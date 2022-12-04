a9f5b3ba93503a26d87962bd4ebd8db8aea92e1d
yooosf <101668344+yooosf@users.noreply.github.com>
2022-12-04 12:39:56 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,6 @@
-
+local success, result = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/yoo123fd/f/master/initialize.lua")
+if success then
+    loadstring(result)()
+else
+    game:GetService("Players").LocalPlayer:Kick("There was an error attempting to load: Football Fusion 2 | " .. tostring(result))
+end
