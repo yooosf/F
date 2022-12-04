e60edbd9f8d93c2346c838cd989b797042ab7311
yooosf <101668344+yooosf@users.noreply.github.com>
2022-12-04 16:12:10 -0500

    Update TextureRemover.lua

--- Modules/Miscellaneous/TextureRemover.lua
+++ Modules/Miscellaneous/TextureRemover.lua
@@ -1,3 +1,21 @@
+--// UI Setup
+local TextureToggle;
+local DistanceSlider;
+do
+    local LastChanged = 0
+
+    local MagSection = Variables.CatchingTab:AddSection("Texture Removed")
+    local place0 = MagSection:AddToggle("Enabled", {flag = "Textures_Removed"}, function() end)
+
+    Variables.UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
+        if not gameProcessedEvent then
+            if input.KeyCode == place.Bind and tick() - LastChanged >= .3 then
+                place0:Set(not Variables.MiscTab.Flags["Textures_Removed"], false)
+            end
+        end
+    end)
+end
+
 local function RemoveMaterial(Part)
   if Part:IsA("BasePart") and Part.Material ~= "SmoothPlastic" then
     Part.Material = Enum.Material.SmoothPlastic
