a1adfb9fd8ec985defbeaec14e4798659a39e20c
yooosf <101668344+yooosf@users.noreply.github.com>
2022-12-11 10:47:11 -0500

    Update initialize.lua

--- initialize.lua
+++ initialize.lua
@@ -14,7 +14,7 @@ end)
 Variables.Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/yoo123fd/f/master/ui.lua"))()
 Variables.Window = Variables.Library:AddWindow({
     title = {
-        "Underc0ver University",
+        "Example",
         "Football Fusion 2"
     },
     default = true,
@@ -34,9 +34,9 @@ Variables.Physics = Variables.Window:AddTab("Physics")
 Variables.Kicking = Variables.Window:AddTab("Kicking")
 -- // Bootup
 
-Variables.Mags = loadstring(game:HttpGet("https://raw.githubusercontent.com/yoo123fd/f/master/Modules/Catching/Mags.lua"))()
-Variables.Grapher = loadstring(game:HttpGet("https://raw.githubusercontent.com/yoo123fd/f/master/Modules/Catching/Grapher.lua"))()
-Variables.Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/yoo123fd/f/master/Modules/Kicking/Aimbot.lua"))()
-Variables.Boost = loadstring(game:HttpGet("https://raw.githubusercontent.com/yoo123fd/f/master/Modules/Physics/Boost.lua"))()
-Variables.DynamicJump = loadstring(game:HttpGet("https://raw.githubusercontent.com/yoo123fd/f/master/Modules/Physics/DynamicJump.lua"))()
-Variables.Tracer = loadstring(game:HttpGet("https://raw.githubusercontent.com/yoo123fd/f/master/Modules/Catching/Tracer.lua"))()
+Variables.Mags = loadstring(game:HttpGet("https://raw.githubusercontent.com/yooosf/F2F/main/Modules/Catching/Mags.lua"))()
+Variables.Grapher = loadstring(game:HttpGet("https://raw.githubusercontent.com/yooosf/F2F/main/Modules/Catching/Grapher.lua"))()
+Variables.Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/yooosf/F2F/main/Modules/Kicking/Aimbot.lua"))()
+Variables.Boost = loadstring(game:HttpGet("https://raw.githubusercontent.com/yooosf/F2F/main/Modules/Physics/Boost.lua"))()
+Variables.DynamicJump = loadstring(game:HttpGet("https://raw.githubusercontent.com/yooosf/F2F/main/Modules/Physics/DynamicJump.lua"))()
+Variables.Tracer = loadstring(game:HttpGet("https://raw.githubusercontent.com/yooosf/F2F/main/Modules/Catching/Tracer.lua"))()
